587090b488850dd371b62ee9807d1de6fc151800
thechipsweredown <dangductungcfc@gmail.com>
2019-02-13 22:31:25 +0700

    change crf

--- ner_crf/train_ner_crf.py
+++ ner_crf/train_ner_crf.py
@@ -54,7 +54,6 @@ def word2features(sent, i):
     }
     if i > 0:
         word1 = sent[i-1][0]
-        # postag1 = sent[i-1][1]
         features.update({
             '-1:word.lower()': word1.lower(),
             '-1:word.istitle()': word1.istitle(),
@@ -65,7 +64,6 @@ def word2features(sent, i):
 
     if i < len(sent)-1:
         word1 = sent[i+1][0]
-        # postag1 = sent[i+1][1]
         features.update({
             '+1:word.lower()': word1.lower(),
             '+1:word.istitle()': word1.istitle(),
